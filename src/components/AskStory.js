6679fab556104807a7a742bedd4d8a03346a121a
shanushka <anushkashrestha24@gmail.com>
2019-02-04 11:03:21 +0545

    common ui in  storywrapper component

--- src/components/AskStory.js
+++ src/components/AskStory.js
@@ -1,52 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import Tab from './Tab';
+import StoryWrapper from './StoryWrapper';
 import WithFetchData from '../hoc/WithFetchData';
 
-const Job = props => {
-  const listItems = props.stories.map(story => (
-    <li className='storylist' key={story.id}>
-      <a href={story.url}>{story.title}</a>
-      <div>
-        <span> {story.points} points | </span>
-        <span>by {story.by} | </span>
-        <span>approximately {story.time} | </span>
-        <Link to='/comments'>
-          <span className='comments'>
-            comments ({story.commentIdArray ? story.commentIdArray.length : 0})
-          </span>
-        </Link>
-      </div>
-    </li>
-  ));
-
-  if (props.isLoading) {
-    return <p>Loading</p>;
-  }
-
+const AskStory = props => {
   return (
-    <div>
-      <Tab userName = ''/>
-      <div className='button_container clearfix'>
-        <div className='button_right'>
-          <button
-            onClick={props.onPreviousClick}
-            disabled={props.currentPage === 1}
-          >
-            &laquo; Prev
-          </button>
-          <span>{props.currentPage}</span>
-          <button
-            onClick={props.onNextClick}
-            disabled={props.currentPage === props.noOfMaxPage}
-          >
-            Next &raquo;
-          </button>
-        </div>
-      </div>
-      <div className='stories'>{listItems}</div>
-    </div>
-  );};
+    <StoryWrapper
+      stories={props.stories}
+      onPreviousClick={props.onPreviousClick}
+      onNextPreviousClick={props.onNextPreviousClick}
+      noOfMaxPage={props.noOfMaxPage}
+      currentPage={props.currentPage}
+    />
+  );
+};
 
-export default WithFetchData('askstories')(Job);
+export default WithFetchData('askstories')(AskStory);
