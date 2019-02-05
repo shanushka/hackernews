977495d358d38033adbcf0311cf7fc09044f26e9
shanushka <anushkashrestha24@gmail.com>
2019-02-05 17:32:44 +0545

    Updates

---
+++ precache-manifest.823b78299f5d3701d4407ba9ec53500f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86f86b674ca2f80d544f",
+    "url": "/hackernews/static/css/main.4905e19f.chunk.css"
+  },
+  {
+    "revision": "86f86b674ca2f80d544f",
+    "url": "/hackernews/static/js/main.86f86b67.chunk.js"
+  },
+  {
+    "revision": "ca263156fa20fd50965b",
+    "url": "/hackernews/static/js/1.ca263156.chunk.js"
+  },
+  {
+    "revision": "da1602332b801076df13",
+    "url": "/hackernews/static/js/runtime~main.da160233.js"
+  },
+  {
+    "revision": "92421cf55ed8fcddef8214defdb5472d",
+    "url": "/hackernews/static/media/PageNotFound.92421cf5.jpg"
+  },
+  {
+    "revision": "9366b88ab6c577ec2fc4aa06af8481c9",
+    "url": "/hackernews/index.html"
+  }
+];
